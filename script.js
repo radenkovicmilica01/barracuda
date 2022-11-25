a7c0e2e66e313d72ecc9b8cb1a381a50e00fa3ea
radenkovicmilica01 <117443819+radenkovicmilica01@users.noreply.github.com>
2022-11-25 02:12:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,4 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
 }
 
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="K0CuvQwh">
+
